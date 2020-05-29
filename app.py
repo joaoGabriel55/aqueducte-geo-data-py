70df70f36c5e74ad1ef3e39cad90edcae2d6bb7a
joaoGabriel55 <j.quaresmasantos_98@hotmail.com>
2020-05-28 22:03:35 -0300

    Started Auth

--- app.py
+++ app.py
@@ -10,8 +10,16 @@ api = Api(app)
 
 @app.before_request
 def before_request_func():
-    print("before_request is running!")
-    # TODO: AUTH
+    sgeol_instance = request.headers.get('sgeol-instance')
+    user_token = request.headers.get('user-token')
+    print(sgeol_instance, user_token)
+    if user_token == None or sgeol_instance == None:
+        # TODO: AUTH
+        return make_response(
+            jsonify({'message': '\'user-token\' or \'sgeol-instance\' is not present.'}), 401)
+    
+    return make_response(
+            jsonify({'message': 'You do not have \'gerente\' role to access API'}), 401)
 
 
 api.add_resource(Upload_Geo_Files, '/upload-geo-files')
